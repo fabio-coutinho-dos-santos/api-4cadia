5b59b7da638e7372581acf894fd465fb71d607db
FabioSantCou <fabio.santcou@gmail.com>
2022-04-06 07:20:22 -0300

    update return messages in register functions

--- src/Model/User/authService.js
+++ src/Model/User/authService.js
@@ -48,19 +48,19 @@ const signup = (req, res, next) => {
 
 
 	if(!email.match(emailRegex)) {
-		return res.status(400).send({errors: ["O e-mail informado está inválido"]})
+		return res.status(400).send({errors: ["Invalid email"]})
 	}
 
 	if(!password.match(passwordRegex)) {
 		return res.status(401).send({errors: [
-			"A senha precisar ter pelo menos um número e tamanho entre 6-12 caracteres."
+			"Invalid password"
 		]})
 	}
 
 	const salt = bcrypt.genSaltSync()
 	const passwordHash = bcrypt.hashSync(password, salt)
 	if(!bcrypt.compareSync(confirmPassword, passwordHash)) {
-		return res.status(402).send({errors: ["Senhas não conferem."]})
+		return res.status(402).send({errors: ["Passwords don't match"]})
 	}
 
 	User.findOne({email}, (err, user) => {
@@ -68,7 +68,7 @@ const signup = (req, res, next) => {
 		if(err) {
 			return sendErrorsFromDB(res, err)
 		} else if (user) {
-			return res.status(403).send({errors: ["Usuário já cadastrado."]})
+			return res.status(403).send({errors: ["This user is already registered"]})
 		} else {
 			const newUser = new User({ name, email, password: passwordHash, profile})
 			newUser.save(err => {
