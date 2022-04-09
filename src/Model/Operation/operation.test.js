e6fe53265884e29c2f05639c7044e6eb2247c3f8
FabioSantCou <fabio.santcou@gmail.com>
2022-04-09 00:07:52 -0300

    added tests with wrong date and time values

--- src/Model/Operation/operation.test.js
+++ src/Model/Operation/operation.test.js
@@ -40,6 +40,7 @@ test("Test Save Operation",()=>{
 		.post("/api/operation/save")
 		.set(commonHeaders)
 		.send({
+			idUser:"624d71ff3526da02eacab759",
 			code:"1234",
 			description:"Test",
 			amount:50,
@@ -50,12 +51,135 @@ test("Test Save Operation",()=>{
 		.then(response => {
 			expect(response.status).toBe(201)
 			expect(response.body._id).toBeDefined()
+			expect(response.body.idUser).toBe("624d71ff3526da02eacab759")
 			expect(response.body.code).toBe("1234")
 			expect(response.body.description).toBe("Test")
 			expect(response.body.amount).toBe(50)
 			expect(response.body.date).toBe("05/06/2022")
 			expect(response.body.hour).toBe("20:00")
 			expect(response.body.type).toBe("Credit")
+		})
+})
+
+test("Test Save Operation with wrong hour",()=>{
+	
+	var commonHeaders = { 
+		"authorization":globalToken,
+	}
+
+	return request (URL_TEST)
+		.post("/api/operation/save")
+		.set(commonHeaders)
+		.send({
+			idUser:"624d71ff3526da02eacab759",
+			code:"1234",
+			description:"Test",
+			amount:50,
+			date:"05/06/2022",
+			hour:"24:00",
+			type:"Credit"
+		})
+		.then(response => {
+			expect(response.status).toBe(400)
+		})
+})
+
+test("Test Save Operation with wrong minutes",()=>{
+	
+	var commonHeaders = { 
+		"authorization":globalToken,
+	}
+
+	return request (URL_TEST)
+		.post("/api/operation/save")
+		.set(commonHeaders)
+		.send({
+			idUser:"624d71ff3526da02eacab759",
+			code:"1234",
+			description:"Test",
+			amount:50,
+			date:"05/06/2022",
+			hour:"23:60",
+			type:"Credit"
+		})
+		.then(response => {
+			expect(response.status).toBe(400)
+			expect(response.body.errors).toBe("Invalid hour")
+		})
+})
+
+test("Test Save Operation with wrong day",()=>{
+	
+	var commonHeaders = { 
+		"authorization":globalToken,
+	}
+
+	return request (URL_TEST)
+		.post("/api/operation/save")
+		.set(commonHeaders)
+		.send({
+			idUser:"624d71ff3526da02eacab759",
+			code:"1234",
+			description:"Test",
+			amount:50,
+			date:"05/32/2022",
+			hour:"23:59",
+			type:"Credit"
+		})
+		.then(response => {
+			expect(response.status).toBe(400)
+			expect(response.body.errors).toBe("Invalid date")
+
+		})
+})
+
+test("Test Save Operation with wrong month",()=>{
+	
+	var commonHeaders = { 
+		"authorization":globalToken,
+	}
+
+	return request (URL_TEST)
+		.post("/api/operation/save")
+		.set(commonHeaders)
+		.send({
+			idUser:"624d71ff3526da02eacab759",
+			code:"1234",
+			description:"Test",
+			amount:50,
+			date:"13/30/2022",
+			hour:"23:59",
+			type:"Credit"
+		})
+		.then(response => {
+			expect(response.status).toBe(400)
+			expect(response.body.errors).toBe("Invalid date")
+
+		})
+})
+
+test("Test Save Operation with wrong date and time",()=>{
+	
+	var commonHeaders = { 
+		"authorization":globalToken,
+	}
+
+	return request (URL_TEST)
+		.post("/api/operation/save")
+		.set(commonHeaders)
+		.send({
+			idUser:"624d71ff3526da02eacab759",
+			code:"1234",
+			description:"Test",
+			amount:50,
+			date:"13/30/2022",
+			hour:"23:60",
+			type:"Credit"
+		})
+		.then(response => {
+			expect(response.status).toBe(400)
+			expect(response.body.errors).toBe("Invalid date and hour")
+
 		})
 })
 
@@ -78,6 +202,21 @@ test("Test Calculate balance",()=>{
 		})
 })
 
+
+
+test("Test Calculate balance with wron token",()=>{
+	
+	var commonHeaders = { 
+		"authorization":"asdasdasdasd",
+	}
+
+	return request (URL_TEST)
+		.get("/api/operation/balance")
+		.set(commonHeaders)
+		.then(response => {
+			expect(response.status).toBe(403)
+		})
+})
 //==================================================================================================================================================================================================
 
 //======================================================================================== Test calculate Balance ==================================================================================
